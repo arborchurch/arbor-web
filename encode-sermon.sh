0dc26180ef7c93f1dc3f48634753203783bcd681
Jonathan McPherson <jonathan@rstudio.com>
2017-04-02 21:20:45 -0700

    bump up the volume on the podcast encodings

--- encode-sermon.sh
+++ encode-sermon.sh
@@ -6,6 +6,7 @@ ffmpeg -i "$3" \
        -c:a aac \
        -b:a 96k \
        -ac 1 \
+       -af "volume=20dB" \
        -metadata title="$1" \
        -metadata author="$2" \
        -metadata year=2017 \
