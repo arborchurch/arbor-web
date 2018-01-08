9582a79c434db9bc638a26e5b99e6bb1ff150ca1
Jonathan McPherson <jonathan@rstudio.com>
2018-01-07 20:14:24 -0800

    upload sermon right after encoding it

--- encode-sermon.sh
+++ encode-sermon.sh
@@ -2,6 +2,7 @@
 # Usage:
 # encode-sermon.sh "sermon title" "speaker" wav m4a
 
+# encode to m4a
 ffmpeg -i "$3" \
        -c:a aac \
        -b:a 96k \
@@ -9,10 +10,14 @@ ffmpeg -i "$3" \
        -af "volume=20dB" \
        -metadata title="$1" \
        -metadata author="$2" \
-       -metadata year=2017 \
+       -metadata year=2018 \
        -movflags +faststart \
        "$4"
 
+# emit useful information
+stat "$4"
 
+# upload to web host
+scp "$4" arborchurch@arborchurch.com:arborchurch.com/podcast
 
 
