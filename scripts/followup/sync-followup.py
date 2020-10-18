97180486cf0b421bfeddbe4bbd4b6bb512e2ab1a
Jonathan McPherson <jonathan@rstudio.com>
2020-10-18 14:24:52 -0700

    emit metadata for sanity

--- scripts/followup/sync-followup.py
+++ scripts/followup/sync-followup.py
@@ -3,17 +3,22 @@
 import urllib.request
 import xml.etree.ElementTree as ET
 
+print("Synchronizing playlist from YouTube...")
+
 # get the RSS XML from YouTube
 response = urllib.request.urlopen(
     'https://www.youtube.com/feeds/videos.xml?playlist_id=PLBA3HWqIJ2eos8Ikf0Fwj33W_WiIShBS5')
 rss = response.read()
 
 root = ET.fromstring(rss)
-for child in root:
-    if child.tag == "entry":
-        id = child.find("yt:videoId")
-        print(child.text)
-
+for child in root.findall("{http://www.w3.org/2005/Atom}entry"):
+    id = child.find("{http://www.youtube.com/xml/schemas/2015}videoId").text
+    title = child.find("{http://www.w3.org/2005/Atom}title").text
+    date = child.find("{http://www.w3.org/2005/Atom}published").text
+    media = child.find("{http://search.yahoo.com/mrss/}group")
+    description = media.find("{http://search.yahoo.com/mrss/}description").text
+    print(title + " (" + id + "), published " + date)
+    
 # TODO: Get followup episodes from YAML, find IDs
 # If an ID exists on youtube but not in YAML:
 # 1. youtube-dl to get its video file audio in FLAC
